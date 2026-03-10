5e3572311dae7fa904453713559cd2d9c32a59ec
Aidan <sima0133@algonquinlive.com>
2026-03-10 17:29:51 -0400

    Upgraded to version 1.1

--- dice_roller.py
+++ dice_roller.py
@@ -5,22 +5,39 @@ Program Name: dice_roller.py
 Purpose: A randomized dice roller game.
 Author: Aidan Simard
 Date: 3/10/2026
-Version: 1.0
+Version: 1.1
 """
 
 import random
-# This part is just asking for the inputs for how many dice's there will be and how many faces each die has
-num_dice = int(input("Enter the number of dice: "))
-die_faces  = int(input("Enter the number of dice faces: "))
 
-# this part is basically summarizing the results: the min value is for every one dice and the max value multiples by the die number and face.
-min_val = num_dice * 1
-max_val = num_dice * die_faces
+while True:
+    num_dice = 0
+    while num_dice <= 0:
+        num_dice = int(input("Enter a number of dice(must be greater than 0): "))
+        if num_dice <= 0:
+            print("Invalid input. Please put a prompt greater than 0.")
 
-# roll result is basically a variable being created to show the results.
-roll_result = random.randint(min_val, max_val)
+    die_faces = 0
+    while die_faces <= 0:
+        die_faces = int(input("Enter the number of faces(must be greater than 0):"))
+        if die_faces <= 0:
+            print("Invalid input. Please put a prompt greater than 0.")
 
-print("The result of the dice roll is: ", roll_result)
+        min_val = num_dice * 1
+        max_val = num_dice * die_faces
 
-if roll_result == max_val:
-    print("Congratulations, you rolled the max value!")
+        roll_result = random.randint(min_val, max_val)
+
+        print("The result of of your roll:" , roll_result)
+
+        if roll_result == max_val:
+            print("You rolled the max value!")
+        elif roll_result == min_val:
+            print("You rolled the min value!")
+
+    repeat = input ("Do you want to roll again? (y/n): ")
+    if repeat == "y":
+        print("Roll Again!")
+
+    elif repeat == "n":
+        break
